007f6f01424d94a39da2141a553cd8f098cacec6
MdDubey <md@whatever.com>
2014-06-28 15:05:12 +0530

    Functions can be used in other apps pulled out from script.js

--- timesheet/scripts.js
+++ timesheet/scripts.js
@@ -1,83 +1,19 @@
-var infoCount = 1;
-var errorCount = 1;
-var pageCount = 1;
-var fs = require('fs');
 var firstTimeSignIn = true;
 var system = require('system');
 var evaluator = require('./evaluator');
-
-function setup(){
-    fs.write('./logs/url.json','','w');
-    fs.write('./logs/pages.log','','w');
-}
-
-setup();
-var getDocumentDetails = function(document){
-    var details = {};
-    details.title = document.title;
-    details.url = window.location.href;
-    details.content = document.getElementsByTagName('html')[0].outerHTML;
-    return details;
-}
-
-function renderPageAsInfo(name) {
-    var imageName = './screens/info/COUNT_NAME.png';
-    imageName = imageName.replace('COUNT',infoCount++).replace('NAME',name);
-    page.render(imageName);
-}
-
-function renderPageAsError(name){
-    var imageName = './screens/error/COUNT_NAME.png';
-    imageName = imageName.replace('COUNT',errorCount++).replace('NAME',name);
-    page.render(imageName);
-}
-
-function getTime () {
-    return new Date().toString().split(" ")[4]; //current system time
-}
-
-var storePageURL = function(pageDetails){
-    var log = 'Page {{TITLE}} opened with url {{PAGEURL}} at {{TIME}}\n';
-    log = log.replace('TITLE', pageDetails.title).replace('PAGEURL', pageDetails.url).replace('TIME', getTime());
-    var url = {};
-    url[pageDetails.title] = pageDetails.url;
-    fs.write('./logs/url.json',JSON.stringify(url)+'\n','a');
-    fs.write('./logs/pages.log',log,'a');
-}
-
-var storePageContent = function(pageDetails){
-    var path = './pages/COUNT_FILENAME.html';
-    path = path.replace('FILENAME',pageDetails.title).replace('COUNT',pageCount++);
-    path = path.replace(':','-');
-    fs.write(path,pageDetails.content,'w');
-}
-
-var storePageDetails = function(pageDetails){
-    storePageContent(pageDetails);
-    storePageURL(pageDetails);
-}
-
-var eventFire = function (el, etype) {
-    if (el.fireEvent) {
-        (el.fireEvent('on' + etype));
-        return;
-    }    
-    var evObj = document.createEvent('Events');
-    evObj.initEvent(etype, true, false);
-    el.dispatchEvent(evObj);
-};
+var myLib = require('../library/library');
 
 exports.performOperationOnConfirm = function(msg){
     if(msg == 'Submit timecards for approval? You will not be able to make changes once they are submitted.'){
         console.log('confirm on page:--',msg);
-        renderPageAsInfo('clickedSubmit');
+        myLib.renderPageAsInfo('clickedSubmit');
         return true;
     }
 };
 
 exports.performOperationOnError = function(error){
     console.log('Error occured '+error);
-    renderPageAsError('error');
+    myLib.renderPageAsError('error');
 };
 
 exports.performOperationOnAlert = function(msg){
@@ -96,33 +32,33 @@ exports.performOperationOnPrompt = function(msg){
 exports.performOperationOnConsoleMessage = function (msg) {
     var operations = {};
     operations['login'] = function(){
-        renderPageAsInfo('filledLoginForm');
+        myLib.renderPageAsInfo('filledLoginForm');
     };
 
     operations['verify'] = function(){
         console.log('Verification code sent on your mobile');
-        renderPageAsInfo('afterCodeSent');
+        myLib.renderPageAsInfo('afterCodeSent');
     };
 
     operations['code filled'] = function(){
-        renderPageAsInfo('afterCodeFilled');
+        myLib.renderPageAsInfo('afterCodeFilled');
     };
 
     operations['CFPW'] = function(){
-        renderPageAsInfo('proceedToCopyFromPreviousWeek');
+        myLib.renderPageAsInfo('proceedToCopyFromPreviousWeek');
     };
 
     operations['copied'] = function(){
-        renderPageAsInfo('copiedFromPrevWeek');
+        myLib.renderPageAsInfo('copiedFromPrevWeek');
     };
 
     operations['filledSheets'] = function(){
-        renderPageAsInfo('filledTimeSheets');
+        myLib.renderPageAsInfo('filledTimeSheets');
+        phantom.exit();
     };
 
     operations['submitted'] = function(){
-        renderPageAsInfo('submittedTimeCard');
-        phantom.exit();
+        myLib.renderPageAsInfo('submittedTimeCard');
     };
 
     var showConsoleMessage = function(){
@@ -141,37 +77,37 @@ exports.performOperationOnPageLoaded = function(args){
             if(!firstTimeSignIn)
                 return;
             console.log('opening login page');
-            renderPageAsInfo('login');
-            var pageDetails = page.evaluate(evaluator.onLoginPage, args, getDocumentDetails);
-            storePageDetails(pageDetails);
+            myLib.renderPageAsInfo('login');
+            var pageDetails = page.evaluate(evaluator.onLoginPage, args, myLib.getDocumentDetails);
+            myLib.storePageDetails(pageDetails);
         };
 
         actions['ThoughtWorks - Extra Verification'] = function(){
-            renderPageAsInfo('verify');
+            myLib.renderPageAsInfo('verify');
             console.log('loaded Verification page');
-            var pageDetails = page.evaluate(evaluator.onVerificationPage, eventFire, getDocumentDetails);
-            storePageDetails(pageDetails);
+            var pageDetails = page.evaluate(evaluator.onVerificationPage, myLib.eventFire, myLib.getDocumentDetails);
+            myLib.storePageDetails(pageDetails);
         };
 
         actions['salesforce.com - Unlimited Edition'] = function(){
-            renderPageAsInfo('ourThoughtworksHome');
+            myLib.renderPageAsInfo('ourThoughtworksHome');
             console.log('Loading Our ThoughtWorks Page');
-            var pageDetails = page.evaluate(evaluator.onOurThoughtworksHomePage, eventFire, getDocumentDetails);
-            storePageDetails(pageDetails);
+            var pageDetails = page.evaluate(evaluator.onOurThoughtworksHomePage, myLib.eventFire, myLib.getDocumentDetails);
+            myLib.storePageDetails(pageDetails);
         };
 
         actions['Timecards: Home ~ salesforce.com - Unlimited Edition'] = function(){
-            renderPageAsInfo('timecardsHome');
+            myLib.renderPageAsInfo('timecardsHome');
             console.log('loading Timecards Home Page');
-            var pageDetails = page.evaluate(evaluator.onTimeCardsHomePage, getDocumentDetails);
-            storePageDetails(pageDetails);
+            var pageDetails = page.evaluate(evaluator.onTimeCardsHomePage, myLib.getDocumentDetails);
+            myLib.storePageDetails(pageDetails);
         }
 
         actions['Timecard Entry ~ salesforce.com - Unlimited Edition'] = function(){
-            renderPageAsInfo('timecardsEntry');
+            myLib.renderPageAsInfo('timecardsEntry');
             console.log('loading Timecard Entry Page');
-            var pageDetails = page.evaluate(evaluator.onTimeCardsEntryPage, args[2], getDocumentDetails, eventFire);
-            storePageDetails(pageDetails);
+            var pageDetails = page.evaluate(evaluator.onTimeCardsEntryPage, args[2], myLib.getDocumentDetails, myLib.eventFire);
+            myLib.storePageDetails(pageDetails);
         }
 
         actions[page.title] && actions[page.title]();
